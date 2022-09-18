2f1347639fd0f8ded55b035c2b82a2bd4d2971ce
Theo Sokos <theo.sokos@rabobank.com>
2022-09-18 23:33:31 +0100

    First commit - Added worker service

---
+++ WorkerService/WorkerService/Program.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Threading.Tasks;
+
+
+namespace WorkerService
+{
+    public class Program
+    {
+        private static ILogger<Program>? Logger { get; set; }
+        public static async Task Main(string[] args)
+        {
+            // Identify environment
+            var appEnvironment = Environment.GetEnvironmentVariable("WorkerServiceVariable") ?? "Development";
+          
+            var configuration = new ConfigurationBuilder()
+                .AddEnvironmentVariables()
+                .AddCommandLine(args)
+                .AddJsonFile($"appsettings.{appEnvironment}.json")
+                .Build();
+            
+            var host = Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration(builder =>
+                {
+                    builder.Sources.Clear();
+                    builder.AddConfiguration(configuration);
+                })
+                .ConfigureServices((context, services) =>
+                {
+                    services.AddHostedService<Worker>();
+                })
+                .Build();
+
+            Logger = host.Services.GetRequiredService<ILogger<Program>>();
+
+            Logger.LogInformation($"Application stating");
+
+            Logger.LogInformation($"Loading configuration for environment: {appEnvironment}");
+
+
+            try
+            {
+                await host.RunAsync();
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError($"INNER EXCEPTION  :   {ex.Message}");
+                Logger.LogError($"STACK TRACE      :   {ex.StackTrace}");
+                Environment.Exit(1);
+            }
+
+            // Success .. exit 
+            Logger.LogInformation("Program successfully completed!");
+            Environment.Exit(0);
+        }
+    }
+}
---
+++ WorkerService/WorkerService/Worker.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WorkerService
+{
+    public class Worker : BackgroundService
+    {
+        private readonly ILogger<Worker> _logger;
+        private readonly IConfiguration _configuration;
+
+        public Worker(ILogger<Worker> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+    
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            var runFrequency =  _configuration.GetValue<int>("WorkerConfig:RunFrequency");
+
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                _logger.LogInformation($"Worker running at: {DateTimeOffset.Now}, next run in {runFrequency} msec");
+                await Task.Delay(runFrequency, stoppingToken);
+            }
+        }
+    }
+}
